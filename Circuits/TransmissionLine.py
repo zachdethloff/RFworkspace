a5e0056cb66e138c8514869df3c8738c0b089fc6
zachdethloff <zachdethloff@gmail.com>
2025-09-12 20:21:26 -0400

    Bare minimum complete

--- Circuits/TransmissionLine.py
+++ Circuits/TransmissionLine.py
@@ -15,8 +15,9 @@ def V(z: int, wl: float, rc: float):
 
 Zo = 50
 Load = [25,50,75,100]
-z = np.arange(0,5,100)
+z = np.arange(0,5,1/100)
 wl = 0.3
+plt.figure(figsize=(10,6))
 
 for Zl in Load:
     vrange = []
@@ -24,6 +25,18 @@ for Zl in Load:
         rc = (Zl - Zo)/(Zl + Zo)
         vmag = np.abs(V(x,wl,rc))
         vrange.append(vmag)
+    print("VSWR for Impedance Load of " + str(Zl) + " Ohms = " + str((1+np.abs(rc))/(1-np.abs(rc))) + "\n")
+    plt.plot(z,vrange,label=str(Zl) + " Ohms")
+
+plt.legend()
+plt.title("Voltage Down A Transmission Line With Characteristic Impedance of 50 Ohms")
+plt.xlabel("Distance (m)")
+plt.ylabel("Voltage (V)")
+plt.savefig('my_plot.png', dpi=300, bbox_inches='tight')
+plt.close()
+print('Plot has been saved')
+
+
         
 
 
