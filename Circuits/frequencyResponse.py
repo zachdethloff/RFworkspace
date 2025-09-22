57404136073105033a669724f5e5375d4c0ee135
zdethloffice <zachary.dethloff@iceye.us>
2025-09-22 23:44:54 +0000

    Finished low pass hard work

--- Circuits/frequencyResponse.py
+++ Circuits/frequencyResponse.py
@@ -26,8 +26,8 @@ for f in fs:
     Z_C = -1j/(w*C)
     Hmag = np.sqrt(np.real(H(Z_R,Z_C))**2 + np.imag(H(Z_R,Z_C))**2)
     Hmags.append(Hmag)
-    Hphase = np.arctan(np.imag(H(Z_R,Z_C))/np.real(H(Z_R,Z_C)))
-    Hphases.append(Hphase)
+    Hphase = -np.arctan(w*Z_R*C)
+    Hphases.append(Hphase * 180/np.pi)
 
 plt.figure(figsize=(10,6))
 plt.title("Low Pass RC Filter Magnitude Response")
@@ -41,11 +41,20 @@ print('Magnitude Response Plot Saved')
 
 plt.figure(figsize=(10,6))
 plt.title("Low Pass RC Filter Phase Response")
-plt.plot(fs,np.angle(Hphases)*180/np.pi)
-plt.vlines(fc,min(Hphases),max(Hphases),colors='red', linestyles='solid')
-plt.xlabel("Frequency (Hz)")
+plt.plot([f/1e6 for f in fs],Hphases)
+plt.vlines(fc/1e6,min(Hphases),max(Hphases),colors='red', linestyles='solid')
+plt.xlabel("Frequency (MHz)")
 plt.ylabel('Phase Response (Deg)')
 plt.savefig('LPphase.png', dpi=300, bbox_inches='tight')
 plt.close()
 print('Phase Response Plot Saved')
+
+
+############################################
+
+# HIGH PASS FILTER - CR
+
+############################################
+
+
 
