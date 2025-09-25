099cf922f27da0ed59efb9737186a88e6e233fdb
zachdethloff <zachdethloff@gmail.com>
2025-09-24 17:02:50 -0700

    Working on Hp

--- Circuits/frequencyResponse.py
+++ Circuits/frequencyResponse.py
@@ -4,10 +4,36 @@ import matplotlib.pyplot as plt
 def H(Z_1: float, Z_2: float) -> float:
     return Z_2/(Z_1 + Z_2)
 
+def response_finder(fs: list,ftype: str, R: float=0, C: float=0,L: float=0) -> tuple[list[float],list[float]]:
+    Hmags = []
+    Hphases = []
+
+    for f in fs:
+        w = 2*np.pi*f
+        if ftype == 'Low':
+            Z_C = -1j/(w*C)
+            Z_2 = Z_C
+            Z_1 = R
+            Hphase = -np.arctan(w*R*C)
+            Hphases.append(Hphase * 180/np.pi)
+        elif ftype == 'High':
+            Z_C = -1j/(w*C)
+            Z_2 = R
+            Z_1 = Z_C
+            Hphase = np.arctan(w*R*C)
+            Hphases.append(Hphase * 180/np.pi)
+        Hmag = np.sqrt(np.real(H(Z_1,Z_2))**2 + np.imag(H(Z_1,Z_2))**2)
+        Hmags.append(Hmag)
+    return Hmags, Hphases
+
+
 c = 3e8 #m/s
 wls = np.arange(300,1,-1) # meters
 C = 1e-9 # Farads
-Z_R = 50 #Ohms
+R = 50 #Ohms
+fs = [c/wl for wl in wls] # Hz
+fc = 1/(2*np.pi*R*C) # Hz
+print("Frequency Cutoff at " + str(fc) + " Hz")
 
 ############################################ 
 
@@ -15,19 +41,7 @@ Z_R = 50 #Ohms
 
 ############################################
 
-fs = [c/wl for wl in wls]
-Hmags = []
-Hphases = []
-fc = 1/(2*np.pi*Z_R*C)
-print("Frequency Cutoff at " + str(fc) + " Hz")
-
-for f in fs:
-    w = 2*np.pi*f
-    Z_C = -1j/(w*C)
-    Hmag = np.sqrt(np.real(H(Z_R,Z_C))**2 + np.imag(H(Z_R,Z_C))**2)
-    Hmags.append(Hmag)
-    Hphase = -np.arctan(w*Z_R*C)
-    Hphases.append(Hphase * 180/np.pi)
+Hmags, Hphases = response_finder(fs,'Low',R,C)
 
 plt.figure(figsize=(10,6))
 plt.title("Low Pass RC Filter Magnitude Response")
@@ -56,5 +70,24 @@ print('Phase Response Plot Saved')
 
 ############################################
 
+Hmags, Hphases = response_finder(fs,'High',R,C)
 
+plt.figure(figsize=(10,6))
+plt.title("High Pass CR Filter Magnitude Response")
+plt.plot([f/1e6 for f in fs],20*np.log10(np.abs(Hmags)))
+plt.vlines(fc/1e6,20*np.log10(np.abs(min(Hmags))),20*np.log10(np.abs(max(Hmags))),colors='red', linestyles='solid')
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Magnitude Response (dB)')
+plt.savefig('HPmag.png', dpi=300, bbox_inches='tight')
+plt.close()
+print('Magnitude Response Plot Saved')
 
+plt.figure(figsize=(10,6))
+plt.title("High Pass RC Filter Phase Response")
+plt.plot([f/1e6 for f in fs],Hphases)
+plt.vlines(fc/1e6,min(Hphases),max(Hphases),colors='red', linestyles='solid')
+plt.xlabel("Frequency (MHz)")
+plt.ylabel('Phase Response (Deg)')
+plt.savefig('HPphase.png', dpi=300, bbox_inches='tight')
+plt.close()
+print('Phase Response Plot Saved')
